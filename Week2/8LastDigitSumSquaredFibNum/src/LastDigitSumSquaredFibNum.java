345706e10c1dcdaf06e290e3e407ae53ac85268e
Cong Wang <congwang@live.co.uk>
2021-11-13 20:18:03 +0000

    added q8 template

---
+++ Week2/8LastDigitSumSquaredFibNum/src/LastDigitSumSquaredFibNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LastDigitSumSquaredFibNum {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        sumFibSquared(n);
+    }
+
+    public static int sumFibSquared(int n){
+
+    }
+
+}
